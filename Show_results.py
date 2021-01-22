9d6395413776b40068e11469f3254d8b6185f9eb
Erel shtossel <erel.shtossel@live.biu.ac.il>
2021-01-22 12:24:45 +0200

    small fixes

--- Show_results.py
+++ Show_results.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     fig, axs = plt.subplots(1, 4)
     i = 0
     for imagefile in ["cat_easy.jpg", "cat_a.jpg", "cat_yoy.jpg", "cat_medium.jpg"]:
-        image = cv2.cvtColor(cv2.imread(imagefile), cv2.COLOR_BGR2RGB)
+        image = cv2.cvtColor(cv2.imread(f"example cats/{imagefile}"), cv2.COLOR_BGR2RGB)
         axs[i].imshow(image, aspect='auto')
         i += 1
     plt.tight_layout()
@@ -46,26 +46,27 @@ if __name__ == '__main__':
 
 
     # Show all results
-    fig, axs = plt.subplots(3, 10, figsize=(20, 10))
-    i, j = 0, 0
-    for imagefile in sorted_alphanumeric(os.listdir("cat_yoy results")):
-        image = cv2.cvtColor(cv2.imread("cat_yoy results/" + imagefile), cv2.COLOR_BGR2RGB)
-        axs[i][j].imshow(image)
-        axs[i][j].set_yticklabels([])
-        axs[i][j].set_xticklabels([])
-        axs[i][j].set_yticks([])
-        axs[i][j].set_xticks([])
+    for cat in ["cat_yoy results","cat_a results","cat_easy results","cat_medium results"]:
+        fig, axs = plt.subplots(3, 10, figsize=(20, 10))
+        i, j = 0, 0
+        for imagefile in sorted_alphanumeric(os.listdir(cat)):
+            image = cv2.cvtColor(cv2.imread(cat+"/" + imagefile), cv2.COLOR_BGR2RGB)
+            axs[i][j].imshow(image)
+            axs[i][j].set_yticklabels([])
+            axs[i][j].set_xticklabels([])
+            axs[i][j].set_yticks([])
+            axs[i][j].set_xticks([])
 
-        i += 1
-        j += (i) // 3
-        i = (i) % 3
+            i += 1
+            j += (i) // 3
+            i = (i) % 3
 
-    cols = ["boyokov", "diniz", "edmondes", "pp", "sap 1", "sap 2", "BSC 20", "SC 1", "BSC 1", "SC 2"]
-    rows = ['30X30', '100X100', 'Max size']
-    for ax, row in zip(axs[:, 0], rows):
-        ax.set_ylabel(row, rotation=90)
-    for ax, col in zip(axs[0], cols):
-        ax.set_title(col, fontsize=10)
+        cols = ["boyokov", "diniz", "edmondes", "pp", "sap 1", "sap 2", "BSC 20", "SC 1", "BSC 1", "SC 2"]
+        rows = ['30X30', '100X100', 'Max size']
+        for ax, row in zip(axs[:, 0], rows):
+            ax.set_ylabel(row, rotation=90)
+        for ax, col in zip(axs[0], cols):
+            ax.set_title(col, fontsize=10)
 
-    plt.tight_layout()
-    plt.show()
+        plt.tight_layout()
+        plt.show()
